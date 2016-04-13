9ea3e7a1e969440938d3dfc83a835becc1c938a3
Martin Bing <bing.martin.photography@gmail.com>
2016-04-13 16:39:50 +0200

    Fix images on now playing to show image poster

--- web/main.dart
+++ web/main.dart
@@ -3,13 +3,25 @@
 
 import 'dart:html';
 import '../lib/DataService.dart';
+import 'package:di/di.dart' as di;
+import 'package:mdl/mdl.dart';
 
 void main() async {
 
   DataService service = new TMDBService();
 
   List movieList = await service.getNowPlayingMovies();
-  querySelector('#output').text = movieList.length.toString();
-
+  var moviesLen = movieList.length;
+  var divke = new DivElement();
+  movieList.forEach((movie) {
+    print(movie.poster_path);
+    print(movie.id);
+    var divElem = new DivElement();
+    divElem.className = "mdl-card";
+    var imgElem = new ImageElement();
+    imgElem.src = "http://image.tmdb.org/t/p/w185" + movie.poster_path;
+    divElem.append(imgElem);
+    querySelector("#output").append(divElem);
+  });
 }
 
