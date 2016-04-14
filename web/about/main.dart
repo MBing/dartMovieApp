daec196089d15b88935bb8cc74b313b5802d41c3
Martin Bing <bing.martin.photography@gmail.com>
2016-04-14 08:53:56 +0200

    Add some folders for structure

---
+++ web/about/main.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2016, Martin Bing. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'dart:html';
+import 'package:mdl/mdl.dart';
+
+void main() async {
+  registerMdl();
+
+  componentFactory().run().then((_) {
+
+  });
+}
+
--- web/main.dart
+++ web/main.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
+// Copyright (c) 2016, Martin Bing. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:html';
@@ -7,21 +7,22 @@ import 'package:di/di.dart' as di;
 import 'package:mdl/mdl.dart';
 
 void main() async {
-
+  registerMdl();
   DataService service = new TMDBService();
 
   List movieList = await service.getNowPlayingMovies();
-  var moviesLen = movieList.length;
-  var divke = new DivElement();
-  movieList.forEach((movie) {
-    print(movie.poster_path);
-    print(movie.id);
-    var divElem = new DivElement();
-    divElem.className = "mdl-card";
-    var imgElem = new ImageElement();
-    imgElem.src = "http://image.tmdb.org/t/p/w185" + movie.poster_path;
-    divElem.append(imgElem);
-    querySelector("#output").append(divElem);
+  componentFactory().run().then((_) {
+    movieList.forEach((movie) {
+      print(movie.poster_path);
+      print(movie.id);
+      var divElem = new DivElement();
+      divElem.classes.add("mdl-card");
+      divElem.classes.add("mdl-shadow--2dp");
+      var imgElem = new ImageElement();
+      imgElem.src = "http://image.tmdb.org/t/p/w185" + movie.poster_path;
+      divElem.append(imgElem);
+      querySelector("#output").append(divElem);
+    });
   });
 }
 
